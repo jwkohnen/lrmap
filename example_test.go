4626a0dbf42ec073d196d6100f46c613a1721a66
Johannes Kohnen <jwkohnen-github@ko-sys.com>
2021-03-08 19:00:13 +0100

    Make Close() optional, add pooling, iteration, len

---
+++ example_test.go
@@ -0,0 +1,37 @@
+package lrmap_test
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/jwkohnen/lrmap"
+)
+
+func ExampleIterate() {
+	m := lrmap.New()
+
+	for i := 0; i < 10; i++ {
+		k, v := lrmap.Key(i), lrmap.Value(i)
+		m.Set(k, v)
+	}
+
+	m.Flush()
+
+	rh := m.NewReadHandler()
+	rh.Enter()
+	defer rh.Close()
+
+	var values []int
+
+	rh.Iterate(func(_ lrmap.Key, v lrmap.Value) bool {
+		values = append(values, int(v))
+
+		return true
+	})
+
+	sort.Ints(values)
+	fmt.Println(values)
+
+	// Output:
+	// [0 1 2 3 4 5 6 7 8 9]
+}
--- lrmap.go
+++ lrmap.go
@@ -2,6 +2,7 @@ package lrmap
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -10,13 +11,14 @@ import (
 
 type (
 	LRMap struct {
-		mu           sync.Mutex
-		left         mapT
-		right        mapT
-		readMap      unsafe.Pointer
-		writeMap     *mapT
-		opLog        []op
-		readHandlers map[*ReadHandler]struct{}
+		mu              sync.Mutex
+		left            mapT
+		right           mapT
+		readMap         unsafe.Pointer
+		writeMap        *mapT
+		opLog           []op
+		readHandlers    map[*readHandlerInner]struct{}
+		readHandlerPool sync.Pool
 	}
 
 	Key   int
@@ -26,11 +28,14 @@ type (
 )
 
 func New() *LRMap {
+	// nolint:exhaustivestruct
 	m := &LRMap{
 		left:         make(map[Key]Value),
 		right:        make(map[Key]Value),
-		readHandlers: make(map[*ReadHandler]struct{}),
+		readHandlers: make(map[*readHandlerInner]struct{}),
 	}
+
+	m.readHandlerPool.New = func() interface{} { return m.newReadHandler() }
 
 	m.swap()
 
@@ -52,6 +57,7 @@ func (m *LRMap) Delete(k Key) {
 
 	delete(*(m.writeMap), k)
 
+	// nolint:exhaustivestruct
 	m.opLog = append(m.opLog, op{t: opDelete, k: k})
 }
 
@@ -86,6 +92,7 @@ func (m *LRMap) Flush() {
 		case opDelete:
 			delete(*(m.writeMap), o.k)
 		default:
+			// nolint:goerr113
 			panic(fmt.Errorf("op(%d) not implemented", o.t))
 		}
 	}
@@ -95,14 +102,39 @@ func (m *LRMap) Flush() {
 }
 
 func (m *LRMap) NewReadHandler() *ReadHandler {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
-	rh := &ReadHandler{lrm: m}
-
-	m.readHandlers[rh] = struct{}{}
+	rh := m.readHandlerPool.Get().(*ReadHandler)
+	rh.ready = true
 
 	return rh
+}
+
+func (m *LRMap) newReadHandler() *ReadHandler {
+	// Wrap the actual (inner) readHandler in an outer shim and return that shim to the
+	// user and only keep a "weak reference" to the inner readHandler, but not the shim.
+	// If the user drops any references to the outer shim, (eventually) the runtime runs
+	// the finalizer on that shim, so we can call close() on the inner read handler, i.e.
+	// remove the weak reference, enabling the GC to remove the actual read handler as well.
+	//
+	// The reason for doing so is that we can make the user's call to Close() optional.
+	// Calling it is still better, as it makes the resources free to remove for the GC
+	// immediately.  But if the user chooses to use resource management like `sync.Pool` or
+	// possibly other solutions like free-lists, the user may not be able to call Close(),
+	// thus leaking resources through the readHandlers list.
+
+	// nolint:exhaustivestruct
+	inner := &readHandlerInner{lrm: m}
+
+	m.mu.Lock()
+	m.readHandlers[inner] = struct{}{}
+	m.mu.Unlock()
+
+	outer := &ReadHandler{inner: inner}
+	runtime.SetFinalizer(outer, func(rh *ReadHandler) {
+		rh.ready = false
+		rh.inner.close()
+	})
+
+	return outer
 }
 
 func (m *LRMap) swap() {
@@ -123,6 +155,7 @@ func (m *LRMap) swap() {
 		r = &(m.left)
 		w = &(m.right)
 	default:
+		// nolint:goerr113
 		panic(fmt.Errorf("illegal pointer: %v", r))
 	}
 
@@ -131,7 +164,8 @@ func (m *LRMap) swap() {
 }
 
 func (m *LRMap) waitForReaders() {
-	readers := make(map[*ReadHandler]uint64)
+	readers := make(map[*readHandlerInner]uint64)
+
 	for rh := range m.readHandlers {
 		if epoch := atomic.LoadUint64(&(rh.epoch)); epoch%2 == 1 {
 			readers[rh] = epoch
@@ -153,9 +187,11 @@ func (m *LRMap) waitForReaders() {
 
 		time.Sleep(delay)
 
+		const maxDelay = 5 * time.Second
+
 		delay *= 10
-		if delay > 5*time.Second {
-			delay = 5 * time.Second
+		if delay > maxDelay {
+			delay = maxDelay
 		}
 	}
 }
@@ -178,18 +214,70 @@ type op struct {
 }
 
 type ReadHandler struct {
-	lrm    *LRMap
-	live   mapT
-	epoch  uint64
-	closed bool
+	inner *readHandlerInner
+	ready bool
 }
 
-func (r *ReadHandler) Enter() {
-	if r.lrm == nil {
-		panic("reader illegal state: must create with New()")
+func (rh *ReadHandler) Enter()                    { rh.assertReady(); rh.inner.enter() }
+func (rh *ReadHandler) Leave()                    { rh.assertReady(); rh.inner.leave() }
+func (rh *ReadHandler) Get(k Key) Value           { rh.assertReady(); return rh.inner.get(k) }
+func (rh *ReadHandler) GetOK(k Key) (Value, bool) { rh.assertReady(); return rh.inner.getOK(k) }
+func (rh *ReadHandler) Len() int                  { rh.assertReady(); return rh.inner.len() }
+
+func (rh *ReadHandler) Iterate(fn func(k Key, v Value) bool) {
+	rh.assertReady()
+
+	if !rh.inner.entered() {
+		panic("reader illegal state: must call Enter() before iterating")
 	}
 
-	if r.epoch%2 == 1 {
+	for k, v := range rh.inner.live {
+		if ok := fn(k, v); !ok {
+			return
+		}
+	}
+}
+
+func (rh *ReadHandler) Close() {
+	rh.assertReady()
+
+	runtime.SetFinalizer(rh, nil)
+	rh.ready = false
+	rh.inner.close()
+}
+
+func (rh *ReadHandler) Recycle() {
+	if !rh.ready {
+		panic("illegal reader state: must not recycle twice")
+	}
+
+	if rh.inner.entered() {
+		panic("reader illegal state: must call Leave() before recycling")
+	}
+
+	rh.ready = false
+
+	rh.inner.lrm.readHandlerPool.Put(rh)
+}
+
+func (rh *ReadHandler) assertReady() {
+	if rh.inner == nil {
+		panic("reader illegal state: must create with NewReadHandler()") // TODO
+	}
+
+	if !rh.ready {
+		panic(fmt.Errorf("reader illegal state: must not use after Recycle()"))
+	}
+}
+
+type readHandlerInner struct {
+	lrm   *LRMap
+	live  mapT
+	epoch uint64
+}
+
+func (r *readHandlerInner) enter() {
+	if r.entered() {
 		panic("reader illegal state: must not Enter() twice")
 	}
 
@@ -197,36 +285,45 @@ func (r *ReadHandler) Enter() {
 	r.live = r.lrm.getMapAtomic()
 }
 
-func (r *ReadHandler) Leave() {
-	if r.epoch%2 == 0 {
+func (r *readHandlerInner) leave() {
+	if !r.entered() {
 		panic("reader illegal state: must not Leave() twice")
 	}
 
 	atomic.AddUint64(&r.epoch, 1)
 }
 
-func (r *ReadHandler) Get(k Key) Value {
-	v, _ := r.GetOK(k)
+func (r *readHandlerInner) get(k Key) Value {
+	v, _ := r.getOK(k)
+
 	return v
 }
 
-func (r *ReadHandler) GetOK(k Key) (Value, bool) {
-	if r.epoch%2 == 0 {
+func (r *readHandlerInner) getOK(k Key) (Value, bool) {
+	if !r.entered() {
 		panic("reader illegal state: must Enter() before operating on data")
 	}
 
 	v, ok := r.live[k]
+
 	return v, ok
 }
 
-func (r *ReadHandler) Close() {
+func (r *readHandlerInner) len() int {
+	if !r.entered() {
+		panic("reader illegal state: must Enter() before operation on data")
+	}
+
+	return len(r.live)
+}
+
+func (r *readHandlerInner) close() {
 	r.lrm.mu.Lock()
 	defer r.lrm.mu.Unlock()
 
-	if r.closed {
-		panic("reader illegal state: must not Close() more than once")
-	}
-
 	delete(r.lrm.readHandlers, r)
-	r.closed = true
+}
+
+func (r *readHandlerInner) entered() bool {
+	return r.epoch%2 == 1
 }
